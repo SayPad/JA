5b9e9009d22861e917aa6f16401dac04432b4eeb
ramirez <ramirezcoronelcarlos@gmail.com>
2022-05-01 15:45:02 -0400

    mostrar errores de php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $campana = "campana.png";
   require_once 'libs/database.php';
   require_once 'libs/model.php';
